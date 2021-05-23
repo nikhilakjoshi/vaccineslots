7d3f710a23df1f17199d74f0e3074e60aa915c3e
Nikhil Joshi <nikhilakjoshi@gmail.com>
2021-05-23 19:03:53 +0530

    added vaccine type and money sign for paid vaccines. Added chennai and hyderabad

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useCallback, useReducer } from "react";
 import "./App.css";
+import ruppee from "./ruppee.svg";
+import clsx from "clsx";
 
 const initialState = {
   Gulbarga: {
@@ -8,6 +10,10 @@ const initialState = {
   },
   BBMP: {
     name: "BBMP",
+    centers: [],
+  },
+  Chennai: {
+    name: "Chennai",
     centers: [],
   },
   activeWard: "BBMP",
@@ -25,6 +31,18 @@ const reducer = (currState: any, action: any) => {
       return {
         ...currState,
         BBMP: { name: "BBMP", centers: [...action.payload] },
+      };
+    }
+    case "setchennai": {
+      return {
+        ...currState,
+        Chennai: { name: "Chennai", centers: [...action.payload] },
+      };
+    }
+    case "sethyderabad": {
+      return {
+        ...currState,
+        Hyderabad: { name: "Hyderabad", centers: [...action.payload] },
       };
     }
     case "setactiveward": {
@@ -57,8 +75,11 @@ const Slots: React.FC<any> = ({ state, isLoading }) => {
         {state && state.centers && state.centers.length > 0 ? (
           state.centers.map((center: any, index: any) => (
             <div key={index} className="my-8">
-              <div className="font-bold text-xl text-gray-300 underline">
+              <div className="font-bold flex items-center justify-between text-xl text-gray-300 underline">
                 {center.name}
+                {center.fee_type == "Paid" ? (
+                  <img className="h-6" src={ruppee} />
+                ) : null}
               </div>
               <div className="mb-4 text-sm text-gray-400 address">
                 {center.address}
@@ -69,8 +90,13 @@ const Slots: React.FC<any> = ({ state, isLoading }) => {
                     key={ind}
                     className="slotwrap bg-gray-900 text-gray-50 p-4 drop-shadow-sm"
                   >
-                    <div className="date underline text-gray-100 mb-2">
-                      Date: {session.date}
+                    <div className="flex mb-2 items-center">
+                      <div className="date underline text-gray-100">
+                        Date: {session.date}
+                      </div>
+                      <div className="vaccine ml-auto text-sm tracking-widest bg-gray-300 text-green-900 flex items-center px-1 rounded font-bold">
+                        {session.vaccine}
+                      </div>
                     </div>
                     <div className="age ml-4">Age: {session.min_age_limit}</div>
                     <div className="dose1 ml-8">
@@ -97,9 +123,12 @@ const getActiveComp = (state: any) => {
   switch (state.activeWard) {
     case "BBMP":
       return <Slots state={state.BBMP} isLoading={state.isLoading} />;
-
     case "Gulbarga":
       return <Slots state={state.Gulbarga} isLoading={state.isLoading} />;
+    case "Chennai":
+      return <Slots state={state.Chennai} isLoading={state.isLoading} />;
+    case "Hyderabad":
+      return <Slots state={state.Hyderabad} isLoading={state.isLoading} />;
   }
 };
 
@@ -161,6 +190,56 @@ function App() {
       })
     );
 
+    prom.push(
+      new Promise((res) => {
+        fetch(
+          `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=571&date=${new Date().getDate()}-${
+            new Date().getMonth() + 1
+          }-${new Date().getFullYear()}`
+        )
+          .then((resp) => resp.json())
+          .then((resp) => {
+            const temp = resp.centers.filter((center: any) => {
+              let retVal = false;
+              for (const ses of center.sessions) {
+                if (parseInt(ses.min_age_limit) < 45) {
+                  retVal = true;
+                  break;
+                }
+              }
+              return retVal;
+            });
+            dispatch({ type: "setchennai", payload: temp });
+            res("success");
+          });
+      })
+    );
+
+    prom.push(
+      new Promise((res) => {
+        fetch(
+          `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=581&date=${new Date().getDate()}-${
+            new Date().getMonth() + 1
+          }-${new Date().getFullYear()}`
+        )
+          .then((resp) => resp.json())
+          .then((resp) => {
+            const temp = resp.centers.filter((center: any) => {
+              let retVal = false;
+              for (const ses of center.sessions) {
+                if (parseInt(ses.min_age_limit) < 45) {
+                  retVal = true;
+                  break;
+                }
+              }
+              return retVal;
+            });
+            dispatch({ type: "sethyderabad", payload: temp });
+            res("success");
+          });
+      })
+    );
+
     Promise.all(prom).then(() => {
       dispatch({ type: "setLoading", payload: false });
     });
@@ -172,22 +251,26 @@ function App() {
 
   return (
     <div className="App">
-      <header className="bg-gray-900 font-bold text-gray-400 text-xl h-16 flex justify-center items-center border-gray-400 border-b">
-        GLB and BBMP Vaccination slots
+      <header className="bg-gray-900 font-bold text-gray-400 text-xl h-16 flex justify-start px-4 items-center border-gray-400 border-b">
+        Vaccination slots
       </header>
       <div className="wrap">
         <div className="tabroot bg-gray-700 items-center flex gap-4 p-4">
-          {["BBMP", "Gulbarga"].map((d) => (
+          {["BBMP", "Gulbarga", "Chennai", "Hyderabad"].map((d) => (
             <button
               key={d}
-              className="focus:outline-none hover:text-gray-100 text-gray-400"
+              className={clsx({
+                ["focus:outline-none hover:text-gray-100 text-gray-400 text-sm"]:
+                  true,
+                ["underline"]: states.activeWard == d,
+              })}
               onClick={() => dispatch({ type: "setactiveward", payload: d })}
             >
               {d}
             </button>
           ))}
           <button
-            className="focus:outline-none hover:text-gray-100 text-gray-400 ml-auto text-sm"
+            className="focus:outline-none hover:text-gray-100 text-gray-400 text-sm ml-auto"
             onClick={() => LoadData()}
           >
             Refresh
